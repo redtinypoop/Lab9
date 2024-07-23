12e4c90c656cac173940e2563767fa4658e37584
COP3502-Student <161520540+COP3502-Student@users.noreply.github.com>
2024-07-23 15:06:50 -0400

    added decode function

--- Lab9.py
+++ Lab9.py
@@ -6,6 +6,15 @@ def encode(password):
     new_pass =
     print(nums)
 
+def decode(password):
+    decoded = ''
+    for i in range(len(password)):
+        n2 = int(password[i])
+        nn2 = (n2 - 3) % 10
+        decoded += str(nn2)
+    return decoded
+
+
 def main():
     encode('12345555')
 
