9706a8e687486cc567441e54a7ee90390e9f005a
Kaylee Huemura <khuemura@ufl.edu>
2024-07-23 14:43:51 -0400

    added encode

---
+++ Lab9.py
@@ -0,0 +1,12 @@
+def encode(password):
+    nums = []
+    for i in password:
+        num = int(i) + 3
+        nums.append(str(num))
+    new_pass =
+    print(nums)
+
+def main():
+    encode('12345555')
+
+main()
